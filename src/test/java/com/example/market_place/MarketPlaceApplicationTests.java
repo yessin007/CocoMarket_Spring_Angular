24c65b2b36ed9f43cd77066b39418b022a1c9f8f
Yessin Khlif <yessin.khlif@esprit.tn>
2023-01-24 22:42:32 +0100

    Creation_du_Projet

---
+++ src/test/java/com/example/market_place/MarketPlaceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.market_place;
+/*
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
