282d6038bca89b7a1d03453746df1b53f1d28768
maramarfaoui <arfaoui.maram@esprit.tn>
2023-02-19 20:14:11 +0100

    Store+ASS Done

---
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Entity/AfterSaleServices.java
@@ -0,0 +1,28 @@
+package com.example.coco_spring.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Table(name = "AfterSaleServices")
+public class AfterSaleServices implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="serviceId")
+    private Long serviceId;
+    private String description ;
+    @JsonIgnore
+    @ManyToOne
+    Store store ;
+
+
+
+}
---
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Repository/StoreRepository.java
@@ -0,0 +1,10 @@
+package com.example.coco_spring.Repository;
+
+import com.example.coco_spring.Entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store,Long> {
+    Store findBystoreName(String storeName);
+}
