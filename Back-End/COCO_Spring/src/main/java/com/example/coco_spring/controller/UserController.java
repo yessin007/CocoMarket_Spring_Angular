26751aa076c9a7be96e195d357c34137ca217961
Ahmed-lasmar <ahmed.lasmar@esprit.tn>
2023-02-19 16:35:52 +0100

    update signup

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/controller/UserController.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.example.coco_spring.controller;
 
+import com.example.coco_spring.entity.Role;
 import com.example.coco_spring.entity.User;
 import com.example.coco_spring.Service.UserService;
+import com.example.coco_spring.repository.RoleRepository;
+import com.example.coco_spring.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,9 +33,12 @@ public class UserController {
         Optional<User> user = userService.getUserById(id);
         return user.map(value -> ResponseEntity.ok().body(value)).orElseGet(() -> ResponseEntity.notFound().build());
     }
-
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
     @PostMapping("/")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user,@RequestBody Role role) {
+        user.setRoles(Collections.singleton(role));
         userService.createUser(user);
         return ResponseEntity.ok().body(user);
     }
--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/payload/SignUpDto.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/payload/SignUpDto.java
@@ -2,10 +2,18 @@ package com.example.coco_spring.payload;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class SignUpDto {
     private String name;
+    private String LastName;
     private String username;
     private String email;
     private String password;
+    private String Address;
+    private Date DayOfBirth;
+    private String Cin;
+    private String TelNum;
+    private Long roleId;
 }
