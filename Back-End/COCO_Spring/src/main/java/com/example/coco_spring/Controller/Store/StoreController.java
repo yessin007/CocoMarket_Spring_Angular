f25f644e77f3fa075b29e6aa98bd967253cbb468
maramarfaoui <arfaoui.maram@esprit.tn>
2023-02-28 09:35:25 +0100

    store

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Controller/Store/StoreController.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Controller/Store/StoreController.java
@@ -1,6 +1,7 @@
 package com.example.coco_spring.Controller.Store;
 
 import com.example.coco_spring.Entity.*;
+import com.example.coco_spring.Service.Product.ProductServices;
 import com.example.coco_spring.Service.Store.StoreService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 @RequestMapping("/api/store")
 public class StoreController {
     StoreService storeService ;
+    ProductServices productServices ;
     @PostMapping("/addStore")
     public Store add(@RequestBody Store store)  {
 
@@ -40,8 +42,13 @@ public class StoreController {
     public Store findStore(@PathVariable("storeId") Long storeId) {
         return storeService.retrieveItem(storeId);
     }
-    @PostMapping("affectproducttostore/{ids}/{idp}")
-    public void AffectProductToStore(@PathParam("ids") Long storeId,@PathParam("idp")  Long productId){
+    @PostMapping("/affectproducttostore/{ids}/{idp}")
+    public void AffectProductToStore(@PathVariable("ids") Long storeId,@PathVariable("idp")  Long productId){
         storeService.AffectProductToStore(storeId,productId);
+    }
+    @PutMapping("/getProductsByStore/{storeId}")
+    public List<Product> getProductsByStore(@PathVariable("storeId") Long storeId) {
+        return storeService.getProductsByStore(storeId);
+        //return "test";
     }
 }
--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/Store/IMPCocoService.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/Store/IMPCocoService.java
@@ -1,5 +1,6 @@
 package com.example.coco_spring.Service.Store;
 
+import com.example.coco_spring.Entity.Product;
 import com.example.coco_spring.Entity.Store;
 
 import java.util.List;
@@ -7,5 +8,5 @@ import java.util.List;
 public interface IMPCocoService {
     public Store findStoreByName(String storeName);
     public void AffectProductToStore(Long storId ,Long productId);
-
+    public List<Product> getProductsByStore(Long storeId);
 }
