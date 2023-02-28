ecafa2da45ae685cb15adbca690bd6797c11ae56
ahmedmellouli <ahmed.mellouli@esprit.tn>
2023-02-27 16:31:38 -0800

    price_comparaison and affecatataion store_storecatalog

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Repository/StoreRepository.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Repository/StoreRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StoreRepository extends JpaRepository<Store,Long> {
     Store findBystoreName(String storeName);
+
 }
