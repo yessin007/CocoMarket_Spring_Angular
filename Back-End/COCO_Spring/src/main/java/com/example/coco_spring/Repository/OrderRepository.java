594e2e17b6811d863d3f8d7685dbb13ac9f08eb9
Racembenamar <racem.benamar@esprit.tn>
2023-02-28 01:27:01 +0100

    baad nos ellil

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Repository/OrderRepository.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Repository/OrderRepository.java
@@ -9,8 +9,10 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
+    // Show Provider name with their orders
     @Query("SELECT p.providerName as n FROM Provider p JOIN p.deliveries d JOIN d.orders ")
     List<String> findProviderNamesWithOrder();
 
+    //Show Orders list of every Provider
     List<Order> findByDelivery_Provider_ProviderName(String name);
 }
--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/Delivery/IDeliveryService.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/Delivery/IDeliveryService.java
@@ -1,4 +1,6 @@
 package com.example.coco_spring.Service.Delivery;
 
 public interface IDeliveryService {
+    public void assignDeliveryToOrder(Long orderId, Long deliveryId);
+    public void assignProviderDelivery(Long deliveryId, Long providerId);
 }
