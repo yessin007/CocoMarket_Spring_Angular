2487e277d7fc4be343e3fd566857004b61175f90
Yessin Khlif <yessin.khlif@esprit.tn>
2023-03-03 21:49:58 +0100

    payement

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/Payement/PayementService.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/Payement/PayementService.java
@@ -2,12 +2,19 @@ package com.example.coco_spring.Service.Payement;
 import com.example.coco_spring.Entity.*;
 import com.example.coco_spring.Service.*;
 import com.example.coco_spring.Repository.*;
+import com.stripe.Stripe;
+import com.stripe.model.Charge;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -58,5 +65,34 @@ public class PayementService implements ICRUDService<Payement,Long> , IPayementS
         orderRepository.save(order);
 
 
+    }
+
+
+    @Value("k_test_51MfpbjFZqNx8XmmhC1sHRKoes1AgZ98bQTRXs27ztbjZ6X4mBaV5X2oAif5LaLmSkQSqZEi7bUyFe4ukscn5Jjy200DlYLAV3X")
+    private String API_SECET_KEY;
+    public void StripeService() {
+
+    }
+
+    public String createCharge(String email, String token, int amount) {
+
+        String chargeId = null;
+
+        try {
+            Stripe.apiKey = API_SECET_KEY;
+
+            Map<String, Object> chargeParams = new HashMap<>();
+            chargeParams.put("description","Charge for "+email);
+            chargeParams.put("currency","usd");
+            chargeParams.put("amount",amount);
+            chargeParams.put("source",token);
+
+            Charge charge = Charge.create(chargeParams);
+
+            chargeId = charge.getId();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return chargeId;
     }
 }
