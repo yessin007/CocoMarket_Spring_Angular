9e3973eaae04d9a05a1014cf1ca455b967dc343f
Ahmed-lasmar <ahmed.lasmar@esprit.tn>
2023-03-05 11:37:22 +0100

    Update CocoSpringApplication.java
    add Scheduling annotation

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/CocoSpringApplication.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/CocoSpringApplication.java
@@ -3,11 +3,13 @@ package com.example.coco_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
+@EnableScheduling
 @SpringBootApplication
 public class CocoSpringApplication {
     @Bean
