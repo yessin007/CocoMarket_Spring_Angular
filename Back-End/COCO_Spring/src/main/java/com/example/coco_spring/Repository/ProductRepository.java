a77b3e948062a2ed40879057a4ca4482e2e17bfb
Ahmed-lasmar <ahmed.lasmar@esprit.tn>
2023-03-05 18:38:04 +0100

    daily mail: offres of the day

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Repository/ProductRepository.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Repository/ProductRepository.java
@@ -1,16 +1,27 @@
 package com.example.coco_spring.Repository;
 
 import com.example.coco_spring.Entity.*;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByProductName(String productName);
+
+    @Query("SELECT p.productCategory FROM Order o JOIN o.cart.products p WHERE o.payement.user.id = :userId " +
+            "AND o.orderDate BETWEEN :startOfWeek AND :endOfWeek GROUP BY p.productCategory " +
+            "ORDER BY COUNT(p.productCategory) DESC")
+    ProductCategory findTopProductCategoryByUserThisWeek(Long userId, Date startOfWeek, Date endOfWeek);
+    @Query("SELECT p FROM Product p WHERE p.productCategory = :category ORDER BY p.createdAt DESC")
+    List<Product> findTop4ProductsByCategoryOrderByRecentlyAdded(@Param("category") ProductCategory category, Pageable pageable);
+
+
+
 
 }
--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/EmailService.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/EmailService.java
@@ -1,23 +1,38 @@
 package com.example.coco_spring.Service;
 
+import com.example.coco_spring.Entity.Product;
+import com.example.coco_spring.Entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.thymeleaf.TemplateEngine;
-
+import org.slf4j.Logger;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import org.thymeleaf.context.Context;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 @Service
+@Slf4j
 public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
-
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
     @Autowired
     private TemplateEngine templateEngine;
+
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+            "[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
 
     public void sendEmail(String to, String subject, String message) throws MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
@@ -51,6 +66,34 @@ public class EmailService {
 
         messageHelper.setText(content, true);
         mailSender.send(mimeMessage);
+    }
+
+    public void sendDailyOfferEmail(User u, List<Product> p) throws MessagingException {
+        if (StringUtils.isEmpty(u.getEmail())) {
+            logger.error("Recipient address is empty");
+            return;
+        }
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(u.getEmail());
+        if (!matcher.matches()) {
+            logger.error("Invalid recipient email address / the mail is: "+u.getEmail());
+            return;
+        }
+        logger.info("sending daily mail to "+ u.getEmail());
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
+        messageHelper.setSubject("Daily Deals: Get Up to 40% Off on Our Latest Collection");
+        messageHelper.setTo(u.getEmail());
+
+        Context context = new Context();
+        //context.setVariable("subject", subject);
+        //context.setVariable("message", message);
+        String content = templateEngine.process("DailyOffres", context);
+
+
+        messageHelper.setText(content, true);
+        mailSender.send(mimeMessage);
+
     }
 }
 
