1f2ee436ae792be6cc39f8bbfa9bc1f4232c4cc2
maramarfaoui <arfaoui.maram@esprit.tn>
2023-03-10 20:00:28 +0100

    badwod

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/Store/StoreService.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/Store/StoreService.java
@@ -5,13 +5,11 @@ import com.example.coco_spring.Repository.*;
 import com.example.coco_spring.Service.*;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Slf4j
@@ -21,6 +19,12 @@ public class StoreService implements ICRUDService<Store,Long> , IMPCocoService {
     ProductRepository productRepository;
     OrderRepository orderRepository;
     CartRepsitory cartRepsitory;
+    BadWordRepo badWordRepo ;
+    UserRepository userRepo;
+    PostRepo postRepo ;
+    UserDataLoadRepo UserDataLoadRepo;
+    CategoryAdverRepo categoryAdverRepo ;
+    PostCommentRepo postCommentRepo;
     @Override
     public List<Store> findAll() {
 
@@ -68,6 +72,94 @@ public class StoreService implements ICRUDService<Store,Long> , IMPCocoService {
         return store.getProducts();
     }
 
+    public BadWords addBadWord(BadWords b ) {
 
+        return badWordRepo.save(b);
+    }
+    public ResponseEntity<?> addPost(PostStore post, Long IdUser) {
+
+        User u = userRepo.findById(IdUser).orElse(null);
+        DetctaDataLoad(post.getBody(),IdUser);
+        DetctaDataLoad(post.getPostTitle(),IdUser);
+        if (Filtrage_bad_word(post.getBody()) == 0 && Filtrage_bad_word(post.getPostTitle()) == 0) {
+            post.setUser(u);
+            u.getPostStores().add(post);
+            postRepo.save(post);
+            return ResponseEntity.ok().body(post);
+        } else
+            return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY).body("Bads Word Detected");
+    }
+    public int Filtrage_bad_word(String ch) {
+        int x = 0;
+        List<BadWords> l1 = (List<BadWords>) badWordRepo.findAll();
+        for (BadWords badWord : l1) {
+            // if (badWord.getWord().contains(ch))
+            if (ch.contains(badWord.getWord()) == true)
+                x = 1;
+        }
+        return x;
+
+    }
+    public void DetctaDataLoad (String ch , Long idUser) {
+
+        List<UserDataLoad> ul = UserDataLoadRepo.findAll();
+        User u = userRepo.findById(idUser).orElse(null);
+        for (CategoryAdve string : categoryAdverRepo.findAll()) {
+            if (ch.contains(string.getNameCategory())) {
+                if (existDataForUser(string.getNameCategory(),idUser) == true) {
+                    UserDataLoad l = getData(string.getNameCategory(),idUser);
+                    l.setNbrsRequet(l.getNbrsRequet()+1);
+                    UserDataLoadRepo.save(l);
+                }
+                else {
+                    UserDataLoad l1 = new UserDataLoad();
+                    l1.setCategorieData(string.getNameCategory());
+                    l1.setUser(u);
+                    l1.setNbrsRequet(1);
+                    UserDataLoadRepo.save(l1);
+
+                }
+            }
+        }
+    }
+    public Boolean existDataForUser(String ch,Long IdUser) {
+        Boolean x = false;
+        for (UserDataLoad userDataLoad : UserDataLoadRepo.findAll()) {
+            if (userDataLoad.getCategorieData().equals(ch) && userDataLoad.getUser().getId() == IdUser) {
+                x = true;
+            }
+        } return x;
+    }
+    public UserDataLoad getData(String ch,Long IdUser) {
+        UserDataLoad x = null;
+        for (UserDataLoad userDataLoad : UserDataLoadRepo.findAll()) {
+            if (userDataLoad.getCategorieData().equals(ch) && userDataLoad.getUser().getId() == IdUser) {
+                x = userDataLoad;
+            }
+        }
+        return x;
+    }
+    public ResponseEntity<?> addComment_to_Post(PostComment postComment, Long idPost, Long idUser) {
+        PostStore p = postRepo.findById(idPost).orElse(null);
+        User u = userRepo.findById(idUser).orElse(null);
+        DetctaDataLoad(postComment.getCommentBody(),idUser);
+        if (Filtrage_bad_word(postComment.getCommentBody()) == 0) {
+            postComment.setUser(u);
+            postComment.setPostStore(p);
+
+            postCommentRepo.save(postComment);
+            return ResponseEntity.ok().body(postComment);      }else
+            /*
+             * Set<PostComment> pc = p.getPostComments(); pc.add(postComment);
+             * p.setPostComments(pc); postRepo.save(p);
+             *
+             * Set<PostComment> pu = u.getPostComments(); pu.add(postComment);
+             * u.setPostComments(pu); userRepo.save(u);
+             *
+             *
+             */
+            //}
+            return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY).body("Bads Word Detected");
+    }
 
 }
