77bf43e37f6df862250169c9cb63cbdc5df881ed
Ahmed-lasmar <91993395+Ahmed-lasmar@users.noreply.github.com>
2023-03-13 11:27:35 +0100

    mail

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/EmailService.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/Service/EmailService.java
@@ -54,10 +54,11 @@ public class EmailService {
     public void sendWelcomeEmail(User u) throws MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
-        messageHelper.setSubject("Welcome");
+        messageHelper.setSubject("Welcome To COCO Market");
         messageHelper.setTo(u.getEmail());
 
         Context context = new Context();
+        context.setVariable("name", u.getName());
         //String content = templateEngine.process("email-template", context);
         String content = templateEngine.process("welcomeMail", context);
 
