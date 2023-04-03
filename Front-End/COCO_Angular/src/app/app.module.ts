28ca657b19bae215ad88aaa07067243b3b7466d3
Ahmed-lasmar <ahmed.lasmar@esprit.tn>
2023-04-04 00:28:51 +0100

    d

--- Front-End/COCO_Angular/src/app/app.module.ts
+++ Front-End/COCO_Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnderConstructionComponent } from './demo/under-construction/under-construction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnderConstructionComponent
   ],
   imports: [
     BrowserModule,
