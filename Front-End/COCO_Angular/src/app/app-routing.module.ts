9c27b295c1edfb3d11540cccc219a2ca03132c10
Ahmed Lasmar <ahmed.lasmar@esprit.tn>
2023-04-05 23:33:57 +0100

    add Auth Components

--- Front-End/COCO_Angular/src/app/app-routing.module.ts
+++ Front-End/COCO_Angular/src/app/app-routing.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UnderConstructionComponent} from "./demo/under-construction/under-construction.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {ForgetpasswordComponent} from "./auth/forgetpassword/forgetpassword.component";
+import {ActivateComponent} from "./auth/activate/activate.component";
+import {ResetpasswordComponent} from "./auth/resetpassword/resetpassword.component";
+import {SignupComponent} from "./auth/signup/signup.component";
 
 const routes: Routes = [
-  { path: '**', component : UnderConstructionComponent},
+  { path: '', component: UnderConstructionComponent },
+  { path: 'auth', children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'forgetPassword', component: ForgetpasswordComponent },
+      { path: 'Activate', component: ActivateComponent },
+      { path: 'resetPassword', component: ResetpasswordComponent },
+      { path: 'SignUp', component: SignupComponent },
+    ]},
+  { path: '**', component: UnderConstructionComponent },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- Front-End/COCO_Angular/src/app/app.module.ts
+++ Front-End/COCO_Angular/src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UnderConstructionComponent } from './demo/under-construction/under-construction.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ForgetpasswordComponent } from './auth/forgetpassword/forgetpassword.component';
+import { ResetpasswordComponent } from './auth/resetpassword/resetpassword.component';
+import { ActivateComponent } from './auth/activate/activate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UnderConstructionComponent
+    UnderConstructionComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgetpasswordComponent,
+    ResetpasswordComponent,
+    ActivateComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    RouterModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
