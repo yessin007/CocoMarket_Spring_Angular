123903f1e7888881bc3d48bd5b030e5f6e4bcf86
Ahmed Lasmar <ahmed.lasmar@esprit.tn>
2023-04-27 10:32:28 +0100

    Cors fix

--- Back-End/COCO_Spring/src/main/java/com/example/coco_spring/config/SecurityConfiguration.java
+++ Back-End/COCO_Spring/src/main/java/com/example/coco_spring/config/SecurityConfiguration.java
@@ -1,12 +1,14 @@
 package com.example.coco_spring.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,18 +16,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @ComponentScan
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.cors();
         http
                 .csrf()
                 .disable()
@@ -48,4 +53,18 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+	@Autowired
+	public WebMvcConfigurer corsConfigurer()
+	{
+		return new WebMvcConfigurer()
+		{
+			@Override
+			public void addCorsMappings(CorsRegistry registry)
+			{
+				registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedMethods("*");
+			}
+		};
+	}
 }
