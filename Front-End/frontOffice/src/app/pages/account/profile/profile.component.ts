6a03fc5bec18273139e146a53f003d9c6ac04944
Ahmed Lasmar <ahmed.lasmar@esprit.tn>
2023-04-28 20:21:21 +0100

    profile not completed

--- Front-End/frontOffice/src/app/pages/account/profile/profile.component.ts
+++ Front-End/frontOffice/src/app/pages/account/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../shared/services/auth.service";
+import {Router} from "@angular/router";
+import {User} from '../../../shared/models/User';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+	currentUser: User = new User;
+	isLoggedIn = false;
+
+	rform: any ={
+		name: null,
+		lastName: null,
+		username: null,
+		email: null,
+		cin: null,
+		dayOfBirth: new Date,
+	};
+	constructor( private authService: AuthService, private router: Router) {
+		this.authService.currentUser.subscribe(data => {
+			this.currentUser = data;
+		});
+		if (this.authService.isLoggedIn()){
+			this.isLoggedIn = true;
+		}
+		else this.router.navigate(['']);
+	}
 
   ngOnInit(): void {
+		this.rform.name = this.currentUser.name;
+		this.rform.lastName = this.currentUser.lastName;
+		this.rform.username = this.currentUser.username;
+		this.rform.email = this.currentUser.email;
+		console.info(this.currentUser.dayOfBirth + ' is ' + this.rform.dayOfBirth);
+		// this.rform.dayOfBirth = this.currentUser.dayOfBirth;
+	  this.rform.dayOfBirth = new Date(this.currentUser.dayOfBirth);
+	  this.rform.cin = this.currentUser.cin;
   }
 
+	onSubmit() {
+		console.log("test edit");
+	}
 }
